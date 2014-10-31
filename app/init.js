408776a823552f35fab5da9a76e3a0843fa74c0a
gui <agamennon@gmail.com>
2014-10-31 05:33:27 -0200

    inicio

---
+++ app/init.js
@@ -0,0 +1,25 @@
+var fossnote = angular.module('fossnote', ['mm.foundation','ngRoute','ngAnimate','ngCookies','ngResource'])
+    .config(['$routeProvider', function($routeProvider){
+
+      /*  console.log('--- module & router ---');
+
+        $routeProvider.when('/', {
+            templateUrl: 'views/testes/testes.html',
+            controller: testesCtrl
+        });
+
+        $routeProvider.otherwise({redirectTo: '/'});*/
+
+
+    }]);
+
+
+
+
+function initCtrl($scope,$location,cache,routeAuth,$rootScope,routemain){
+
+
+    console.log('--- init ctrl ---');
+
+
+}
