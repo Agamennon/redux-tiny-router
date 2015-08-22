2749ab2fbcc9f5118a6e49625b7a671d3e5459f7
gui <agamennon@gmail.com>
2015-08-22 03:09:01 -0300

    redux tiny router , clean 2

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,6 @@
 
 
 function handleHashChange(hash){
-    //http://stackoverflow.com/questions/25659960/how-can-i-execute-code-before-hashchange
-     console.log('********************* HASH CHANGE *****************');
-
     return {
         type:'ROUTER_NAVIGATION',
         hash
@@ -13,7 +10,6 @@ function handleHashChange(hash){
 }
 
 function navigateTo(path,search){
-//    console.log('action -> router.hash = '+router.hash);
     console.log(path,search);
     return {
         type:'ROUTER_NAVIGATE_TO',
