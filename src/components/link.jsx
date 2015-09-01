1d2d3756adbaebec12c8b2598b8d2e1475616803
Tommy Jonsson <quazzie@users.noreply.github.com>
2015-09-01 11:47:26 +0200

    Pass href, className and such to Link component.
    Pass props to anchor in link to set className and such, default href to "path?search".

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import qs from 'query-string';
 import * as actions from '../actions/actions.js'
 
 export class Link extends React.Component {
@@ -6,14 +7,17 @@ export class Link extends React.Component {
    static contextTypes = { store: React.PropTypes.any };
 
     click(e){
-        e.preventDefault();
-        this.context.store.dispatch(actions.rtrNavigateTo(this.props.path,this.props.search));
+         e.preventDefault();
+         this.context.store.dispatch(actions.rtrNavigateTo(this.props.path,this.props.search));
     }
 
     render() {
-        return (
-            <a onClick={this.click.bind(this)}>{this.props.children}</a>
-        );
+         const { path, search, ...rest } = this.props;
+         let href = `${path}`;
+         if (search) href = href + `?${qs.stringify(search)}`;
+         return (
+            <a href={href} {...rest} onClick={this.click.bind(this)}>{this.props.children}</a>
+         );
     }
 }
 
